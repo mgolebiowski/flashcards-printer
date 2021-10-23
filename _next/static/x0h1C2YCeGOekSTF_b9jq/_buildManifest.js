7a416348576db195a770b19cf4ee55ba60c07b52
Michal Golebiowski <michal.golabek94@gmail.com>
2021-10-24 01:08:30 +0200

    Deploy the app

--- _next/static/x0h1C2YCeGOekSTF_b9jq/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ea9e7c5ae6ed873dc2f5.css","static/chunks/pages/index-065ef115f842183f0b20.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
