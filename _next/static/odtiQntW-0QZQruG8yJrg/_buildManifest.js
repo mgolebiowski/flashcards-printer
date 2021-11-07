4b4075c744bafecb3afd352deaa4be48a7676110
mgolebiowski <robot@golebiowski.me>
2021-11-07 22:04:07 +0000

    Deploy the app

---
+++ _next/static/odtiQntW-0QZQruG8yJrg/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ed3f0702d24ca4949be6.css","static/chunks/pages/index-6807cb88a91b0350abc3.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
