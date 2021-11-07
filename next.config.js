e75bbeeda6e4fb9705c4b1353abd957e0ab03331
Michal Golebiowski <michal.golabek94@gmail.com>
2021-11-07 23:04:34 +0100

    Add test script

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   reactStrictMode: true,
   assetPrefix: isProd ? '/flashcards-printer' : ''
-}
+};
