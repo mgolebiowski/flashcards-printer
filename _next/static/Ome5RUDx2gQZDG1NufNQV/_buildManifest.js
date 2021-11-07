1e5a397ce4409bda09c6ef409156e074ec8b8e1c
mgolebiowski <robot@golebiowski.me>
2021-11-07 21:56:31 +0000

    Deploy the app

---
+++ _next/static/Ome5RUDx2gQZDG1NufNQV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ed3f0702d24ca4949be6.css","static/chunks/pages/index-268cc2f2207709a3ec08.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/xL6Cd9XwP4kmHDK2Sp8v0/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ea9e7c5ae6ed873dc2f5.css","static/chunks/pages/index-c6d288c5ab00873ac9ba.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
