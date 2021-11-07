627921b326b1bb855309ed61048491c036fb6047
Michal Golebiowski <michal.golabek94@gmail.com>
2021-11-07 23:03:14 +0100

    Add linting

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,6 @@ import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   const [flashcards, setFlashcards] = useState<FlashCard[]>([]);
-  console.log(flashcards)
 
   return (
     <>
